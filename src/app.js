79e3fd30ad538cc124bcc5ebb916b019b0b84a22
Anastakas <kasilova@gmail.com>
2022-09-22 15:50:16 +0200

    add axios link, weather api and showWeather function to the app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+function showTemperature(response) {
+  console.log(response.data);
+}
 
+let apiKey = "d33243fa11c3284dcffcf337fc75caaa";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(showTemperature);
