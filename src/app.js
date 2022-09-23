126f32a3000f527aad57c1cf4939ffff60b3f055
Anastakas <kasilova@gmail.com>
2022-09-23 12:22:57 +0200

    show the actual temperature elements

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 function showTemperature(response) {
   console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].main;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let pressureElement = document.querySelector("#pressure");
+  pressureElement.innerHTML = response.data.main.pressure;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "d33243fa11c3284dcffcf337fc75caaa";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=${units}`;
-console.log(apiUrl);
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Paris&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(showTemperature);
