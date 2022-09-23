36a2872b09c5e4c907771ee3599472255f210daf
Anastakas <kasilova@gmail.com>
2022-09-23 11:01:52 +0200

    change the api url

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ function showTemperature(response) {
 
 let apiKey = "d33243fa11c3284dcffcf337fc75caaa";
 let units = "metric";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=${units}`;
 console.log(apiUrl);
 
 axios.get(apiUrl).then(showTemperature);
